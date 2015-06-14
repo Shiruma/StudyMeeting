ce0028c07a25905bdaf0736a05f705d51dc9914f
Shiruma <nanahosimi23467@gmail.com>
2015-06-14 15:15:11 +0900

    3の倍数で且つ5の倍数の時にfizzbuzzを表示

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,10 @@
 if __name__ == '__main__':
     for i in range(1,101):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            i = "fizzbuzz"
+        elif i % 3 == 0:
             i = "fizz"
         elif i % 5 == 0:
             i = "buzz"
+
         print(i)
