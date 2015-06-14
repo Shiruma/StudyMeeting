02c22cece3bd037b3d440737b107d8d2f60514c6
Shiruma <nanahosimi23467@gmail.com>
2015-06-14 16:20:40 +0900

    取得したイベントのタイトルを表示

--- study_meeting.py
+++ study_meeting.py
@@ -2,3 +2,6 @@ import requests
 
 if __name__ == '__main__':
     data = requests.get('http://connpass.com/api/v1/event/?keyword=python').json()
+    events = data['events']
+    for event in events:
+        print(event['title'])
